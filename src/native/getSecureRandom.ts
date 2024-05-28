3e3459c637fe40c43a121368fc908182f11c6405
Stepan <stepan.patrakeev@guarda.co>
2024-05-28 19:30:24 +0300

    change crypto libs for bare rn compatibility & publish conf.

--- src/native/getSecureRandom.ts
+++ src/native/getSecureRandom.ts
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) Whales Corp. 
+ * Copyright (c) Whales Corp.
  * All Rights Reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-const getRandomBytes = require('expo-random').getRandomBytes;
+const getRandomBytes = require('react-native-randombytes').randomBytes;
 
 export function getSecureRandomBytes(size: number): Buffer {
     return Buffer.from(getRandomBytes(size));
--- src/native/pbkdf2_sha512.ts
+++ src/native/pbkdf2_sha512.ts
@@ -1,15 +1,19 @@
 /**
- * Copyright (c) Whales Corp. 
+ * Copyright (c) Whales Corp.
  * All Rights Reserved.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+const { pbkdf2 } = require("react-native-quick-crypto");
 
 export async function pbkdf2_sha512(key: string | Buffer, salt: string | Buffer, iterations: number, keyLen: number): Promise<Buffer> {
-    const keyBuffer = typeof key === 'string' ? Buffer.from(key, 'utf-8') : key;
-    const saltBuffer = typeof salt === 'string' ? Buffer.from(salt, 'utf-8') : salt;
-    let pbkdf2 = require('react-native-fast-pbkdf2').default;
-    let res = await pbkdf2.derive(keyBuffer.toString('base64'), saltBuffer.toString('base64'), iterations, keyLen, 'sha-512');
-    return Buffer.from(res, 'base64');
+    return new Promise((resolve, reject) => pbkdf2(key, salt, iterations, keyLen, 'sha512', (error: string, derivedKey: Buffer) => {
+        if (error) {
+            reject(error);
+        }
+        else {
+            resolve(derivedKey);
+        }
+    }));
 }
